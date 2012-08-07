7ea92fbdcd86e1e3382c402690b2b5c5a873aa1e
David Osborn <davido@veoviatech.com>
2012-08-06 19:07:19 -0500

    Header and tag line

--- Communappy/Controllers/HomeController.cs
+++ Communappy/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Communappy.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "If you had unlimited developer resources to build a website how would you changes the world?";
 
             return View();
         }
